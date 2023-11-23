f4cfd3dc682c0ade201a2ca0146ea1ff92831c5b
d.kandelov <kaparyaka@mail.ru>
2023-11-24 00:24:36 +0300

    Minor update Binary search

--- Lab1/binary_search.cpp
+++ Lab1/binary_search.cpp
@@ -5,7 +5,7 @@ bool BinarySearch(const std::vector<std::vector<int>>& data, const int& value) {
     size_t height = data.size(), width = data[0].size();
     size_t row = 0, column = width - 1;
 
-    while (row < height && column >= 0) {
+    while (row < height) {
         if (data[row][column] == value) {
             return true;
         }
@@ -33,6 +33,5 @@ bool BinarySearch(const std::vector<std::vector<int>>& data, const int& value) {
             column = left;
         }
     }
-
     return false;
 }
